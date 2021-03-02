0c6c5df32f67e17cf05cf4c8b505de7ff50a3eca
howard <Howard@howards-mac.lan>
2021-03-02 12:50:41 -0500

    started creating flask app and tested in Jupyter but not working in Flask

--- weather_app.py
+++ weather_app.py
@@ -1,7 +1,34 @@
-from flask import Flask
+import numpy as np
+
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+
+from flask import Flask, jsonify
+
+
+#################################################
+# Database Setup
+#################################################
+engine = create_engine("sqlite:///Resources/hawaii.sqlite")
+
+# reflect an existing database into a new model
+Base = automap_base()
+# reflect the tables
+Base.prepare(engine, reflect=True)
+
+# Save reference to the table
+Measurement = Base.classes.measurement
+Station = Base.classes.station
+
+
+# Flask setup
 
 app = Flask(__name__)
 
+
+# Routes setup
 @app.route("/")
 
 def home():
@@ -16,24 +43,34 @@ def home():
     )
 
     
-def precipitation():
+def precipitation("/api/v1.0/precipitation")
     """convert the query results to a dictionary using 'date' as the key and 'prcp' as the value"""
-    
-    return 
+    session = Session(engine)
 
-def stations():
+    year_ago = dt.date(2017, 8, 23) - dt.timedelta(366)
+    precip = session.query(Measurement.date, Measurement.prcp).filter(Measurement.date > year_ago).all()
+
+    prcp_data = []
+    for date, prcp in precip:
+        prcp_dict = {}
+        prcp_dict['date'] = date
+        prcp_dict['prcp'] = prcp
+        prcp_data.append(prcp_dict)
+    return jsonify(prcp_data)
+
+def stations("/api/v1.0/stations")
     """Return JSON list of stations from the dataset"""
     return
 
-def tobs():
+def tobs("/api/v1.0/tobs")
 
     return
 
-def start():
+def start("/api/v1.0/start")
 
     return
 
-def end():
+def end("/api/v1.0/end")
 
     return
 
